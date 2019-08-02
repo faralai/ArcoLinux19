ab177cca05a72e8d4a65dbb4c9ee0da09b0ca2ae
Faralai <faralai@gmail.com>
2019-08-02 12:20:34 +0200

    Add files via upload

---
+++ delete.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+yay -Rns cups-pdf gutenprint cups --noconfirm
+yay -Rns arandr --noconfirm
+yay -Rns atom --noconfirm
+yay -Rns chromium --noconfirm
+yay -Rns evolution --noconfirm
+yay -Rns firefox --noconfirm
+yay -Rns geany --noconfirm
+yay -Rns htop --noconfirm
+yay -Rns inkscape --noconfirm
+yay -Rns meld --noconfirm
+yay -Rns gimp --noconfirm
+yay -Rns qbittorrent --noconfirm
+yay -Rns vim --noconfirm
+yay -Rns vivaldi-codecs-ffmpeg-extra-bin vivaldi --noconfirm
+yay -Rns virtualbox-guest-utils virtualbox-guest-modules-arch --noconfirm
+yay -Rns urxvt-perls rxvt-unicode rxvt-unicode-terminfo urxvt-resize-font-git --noconfirm
+yay -Rns termite --noconfirm
---
+++ postinstall.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+yay -S downgrade --noconfirm --builddir ~/Escritorio/ 
+yay -S discord --noconfirm --builddir ~/Escritorio/ 
+yay -S typora --noconfirm --builddir ~/Escritorio/ 
+yay -S grub-customizer --noconfirm --builddir ~/Escritorio/ 
+yay -S rar --noconfirm --builddir ~/Escritorio/ 
+yay -S seafile-client --noconfirm --builddir ~/Escritorio/  
+yay -S woeusb --noconfirm --builddir ~/Escritorio/ 
+yay -S conky-lua-archers --noconfirm --builddir ~/Escritorio/  
+yay -S package-query --noconfirm --builddir ~/Escritorio/  
+yay -S android-sdk-platform-tools --noconfirm --builddir ~/Escritorio/ 
+yay -S sublime-text2  --noconfirm --builddir ~/Escritorio/  
+yay -S timeshift --noconfirm --builddir ~/Escritorio/ 
+yay -S teamviewer --noconfirm --builddir ~/Escritorio/  
+yay -S buttercup-desktop --noconfirm --builddir ~/Escritorio/  
+yay -S telegram-desktop-bin --noconfirm --builddir ~/Escritorio/ 
+yay -S waterfox-bin waterfox-alpha-bin --noconfirm --builddir ~/Escritorio/  
+yay -S anydesk --noconfirm --builddir ~/Escritorio/ 
+yay -S keepassxc --noconfirm --builddir ~/Escritorio/ 
+yay -S ffmpeg-compat-57 --noconfirm --builddir ~/Escritorio/  
+yay -S spotify --noconfirm --builddir ~/Escritorio/ 
